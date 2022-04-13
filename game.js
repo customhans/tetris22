77fea1bfda7e4ac0565f736028fc38028b93c8c7
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-13 08:08:58 +0200

    moved game loop over to game.js

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
+let dropCounter = 0;
+let dropInterval = 300;
+let lastTime = 0;
+var fr;
+
 const game = {
   start() {
-    console.log("start");
+    this.mainLoop();
   },
 
   drawAll() {
@@ -13,4 +18,24 @@ const game = {
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
   },
-};
+
+  end() {
+    cancelAnimationFrame(fr);
+  },
+
+  mainLoop(time = 0) {
+    const deltaTime = time - lastTime;
+  
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval) {
+      piece.drop();
+    }
+  
+    lastTime = time;
+  
+    game.drawAll();
+    fr = requestAnimationFrame(this.mainLoop.bind(this));
+  }
+}
+
+
