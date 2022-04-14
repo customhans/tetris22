06dc5652ea78860bd71081dd49006fdfb9e10ed3
Hannes Flaschentraeger <hannesflaschentraeger@gmail.com>
2022-04-14 20:20:49 +0200

    add level-up functionality

--- player.js
+++ player.js
@@ -1,5 +1,5 @@
 const player = {
-  level: 5,
+  level: 1,
   time: 0,
   score: 0,
   lines: 0,
@@ -8,10 +8,25 @@ const player = {
     this.lines += clearedRowsCount;
     document.getElementById("lines").textContent = this.lines;
     this.updateScore(SCORE_MAP[clearedRowsCount]);
+
+    this.checkIfLevelUp(clearedRowsCount);
   },
   
   updateScore(addedPoints) {
-    this.score += addedPoints; // * this.player.level
+    this.score += addedPoints * this.level;
     document.getElementById("score").textContent = this.score;
+  },
+
+  checkIfLevelUp(clearedRowsCount) {
+    const newLineCountTens = Math.floor(this.lines / 10);
+    const oldLineCountTens = Math.floor((this.lines - clearedRowsCount) / 10);
+    if (newLineCountTens > oldLineCountTens) {
+      this.levelUp();
+    }
+  },
+
+  levelUp() {
+    this.level++;
+    document.getElementById("level").textContent = this.level;
   }
 }
